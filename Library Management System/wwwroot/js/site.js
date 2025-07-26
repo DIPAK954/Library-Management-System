fe57c34619dcf97c228eb414374b061943b24d3d
dipak <dipakdhrangiwork954@gmail.com>
2025-07-26 22:12:07 +0530

    Hower functionality in Sidebar

--- Library Management System/wwwroot/js/site.js
+++ Library Management System/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+    $(document).ready(function () {
+        $('.sidebar .nav-link').on('click', function () {
+            $('.sidebar .nav-link').removeClass('active'); // Remove from all
+            $(this).addClass('active'); // Add to clicked one
+        });
+    });
